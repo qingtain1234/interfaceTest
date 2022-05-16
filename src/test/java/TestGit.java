ae3806678ca5d990e30bca0318a3567135d2ed2b
qingtian <1056195530@qq.com>
2022-05-16 14:50:15 +0800

    commit

--- src/test/java/TestGit.java
+++ src/test/java/TestGit.java
@@ -6,4 +6,20 @@ public class TestGit {
         System.out.println("aaaa");
         System.out.println("bbb");
     }
+
+    @Test
+    void test123(){
+        System.out.println(removeElement(new int[]{1, 2}, 2));
+    }
+    public int removeElement(int[] nums, int val) {
+        int n=nums.length;
+        int left=0;
+        for(int right=0;right<n;right++){
+            if(nums[right]!=val){
+                nums[left]=nums[right];
+                left++;
+            }
+        }
+        return left;
+    }
 }
